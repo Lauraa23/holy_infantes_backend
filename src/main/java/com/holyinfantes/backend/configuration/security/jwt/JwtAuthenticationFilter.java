a944fdb509dc458e42e640ed7543f44d453eca80
Ruben <ruben.infantes@gmail.com>
2024-11-05 19:04:12 +0100

    Añadidas las librerias y primera parte de la implementacion

---
+++ src/main/java/com/holyinfantes/backend/configuration/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,9 @@
+package com.holyinfantes.backend.configuration.security.jwt;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+
+}
---
+++ src/main/java/com/holyinfantes/backend/configuration/security/jwt/JwtConfig.java
@@ -0,0 +1,14 @@
+package com.holyinfantes.backend.configuration.security.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Setter
+@Getter
+public class JwtConfig {
+    private String secret;
+}
