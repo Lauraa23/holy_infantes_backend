f78d4c2312f7132051fc159002edd0bf0b1c69e4
Laura Pino <lpigueroa@gmail.com>
2024-12-03 19:30:40 +0100

    updateCourse endpoint refactor

--- src/main/java/com/holyinfantes/backend/infrastructure/controllers/course/UpdateCourseController.java
+++ src/main/java/com/holyinfantes/backend/infrastructure/controllers/course/UpdateCourseController.java
@@ -1,7 +1,7 @@
 package com.holyinfantes.backend.infrastructure.controllers.course;
 
 import com.holyinfantes.backend.application.course.UpdateCourse;
-import com.holyinfantes.backend.domain.course.Course;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +15,11 @@ public class UpdateCourseController {
     private UpdateCourse updateCourse;
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> updateCourse(
+    public ResponseEntity<String> updateCourse(
             @PathVariable String id,
             @RequestBody UpdateCourseRequest updateCourseRequest) {
 
-        Course updatedCourse = updateCourse.updateCourse(
+        updateCourse.updateCourse(
                 id,
                 updateCourseRequest.getTitle(),
                 updateCourseRequest.getDescription(),
@@ -27,46 +27,14 @@ public class UpdateCourseController {
                 updateCourseRequest.getVideoUrl()
         );
 
-        return new ResponseEntity<>(updatedCourse, HttpStatus.OK);
+        return new ResponseEntity<>("Course updated successfully", HttpStatus.OK);
     }
 
+    @Data
     public static class UpdateCourseRequest {
         private String title;
         private String description;
         private Long price;
         private String videoUrl;
-
-        // Getters y Setters
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public Long getPrice() {
-            return price;
-        }
-
-        public void setPrice(Long price) {
-            this.price = price;
-        }
-
-        public String getVideoUrl() {
-            return videoUrl;
-        }
-
-        public void setVideoUrl(String videoUrl) {
-            this.videoUrl = videoUrl;
-        }
     }
 }
